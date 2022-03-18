38b0b25f8ca721d87f38cbc477f0762bb4af2ebb
David De La Rosa <davidrr261096@hotmail.com>
2022-03-17 20:32:46 -0400

    creating order and order status entity

---
+++ src/entities/order.entity.js
@@ -0,0 +1,28 @@
+export default class Order {
+  //integer
+  id;
+
+  //string
+  orderStatus;
+
+  //dateTime
+  date;
+
+  //array
+  productList;
+
+  //string
+  orderType;
+
+  //integer
+  userId;
+
+  //string
+  paymentMethod;
+
+  //array
+  itb;
+
+  //integer
+  totalPrice;
+}
